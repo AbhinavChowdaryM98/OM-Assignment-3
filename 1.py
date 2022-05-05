92f001533abe931ed2c97315410c256f4979cee3
AbhinavChowdayM98 <abhinav.chowdary@students.iiit.ac.in>
2022-05-05 12:27:28 +0530

    1st completed at 34 iterations plotting isn't done

--- 1.py
+++ 1.py
@@ -16,20 +16,20 @@ def fun_x2(c,b):
 eps = 0.000001
 
 k = 0
-alpha = [(1,1)]
-x = [(0,0)]
-l = 0
+alpha = [1]
+x = [(0,1)]
+p = []
 beta = 0.1
 tow = 0.7
 while fun_x1(x[k][0],x[k][1])**2 + fun_x2(x[k][0],x[k][1])**2 > eps**2:
-    a = (1,1)
-    while fun(x[k][0] - a[0]*fun_x1(x[k][0],x[k][1]), x[k][1] - a[1]*fun_x2(x[k][0],x[k][1])) < fun(x[k][0],x[k][1]) - (a[0]*beta*fun_x1(x[k][0],x[k][1]) + a[1]*beta*fun_x2(x[k][0],x[k][1])):
-        a[0] *= tow
-        a[1] *= tow
-        l+=1
+    a = 1
+    #while fun(x[k][0] - a[0]*fun_x1(x[k][0],x[k][1]), x[k][1] - a[1]*fun_x2(x[k][0],x[k][1])) < fun(x[k][0],x[k][1]) - (a[0]*beta*fun_x1(x[k][0],x[k][1]) + a[1]*beta*fun_x2(x[k][0],x[k][1])):
+    while fun(x[k][0] - a*fun_x1(x[k][0],x[k][1]), x[k][1] - a*fun_x2(x[k][0],x[k][1])) > fun(x[k][0],x[k][1]) - a*beta*(fun_x1(x[k][0],x[k][1])**2 + fun_x2(x[k][0],x[k][1])**2):
+        a *= tow
     alpha.append(a)
-    x.append((x[-1][0]-a[0]*fun_x1(x[k][0],x[k][1]), x[-1][1]-a[1]*fun_x2(x[k][0],x[k][1])))
+    x.append((x[k][0] - a*fun_x1(x[k][0],x[k][1]), x[k][1] - a*fun_x2(x[k][0],x[k][1])))
     k+=1
+    print(k , '\n', x[k])
 
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
@@ -39,11 +39,12 @@ ax = plt.axes(projection ='3d')
 # a = np.linspace(0,k,k)
 # b = x[a][0]
 # c = x[a][1]
-p = x[:, 0]
-q = x[:, 1]
+'''p = x[:][0]
+q = x[:][1]
+print(p)
 r = fun(p,q)
 ax.plot3D(p, q, r, 'green')
 #ax.set_title('3D line plot geeks for geeks')
 plt.show()
-
+'''
 print("Took ", k, " iterations to converge.")
