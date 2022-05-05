fea8669d8d3c448e1e7f9a77c8c9ce9e015be306
AbhinavChowdayM98 <abhinav.chowdary@students.iiit.ac.in>
2022-05-05 08:53:17 +0530

    3rd all done except optimal apha finding part 1st Error: Numerical result out of range

---
+++ 1.py
@@ -0,0 +1,49 @@
+import math
+import numpy as np
+
+
+
+#Backtracking-Armijo Line Search
+
+def fun(c,b):
+    return math.exp(c + 3*b - 0.1) + math.exp(c - b*3 - 0.1) + math.exp(-1*c - 0.1)
+
+def fun_x1(c,b):
+    return math.exp(c + 3*b - 0.1) + math.exp(c - b*3 - 0.1) - math.exp(-1*c - 0.1)
+
+def fun_x2(c,b):
+    return 3*math.exp(c + 3*b - 0.1) - 3*math.exp(c - b*3 - 0.1)
+eps = 0.000001
+
+k = 0
+alpha = [(1,1)]
+x = [(0,0)]
+l = 0
+beta = 0.1
+tow = 0.7
+while fun_x1(x[k][0],x[k][1])**2 + fun_x2(x[k][0],x[k][1])**2 > eps**2:
+    a = (1,1)
+    while fun(x[k][0] - a[0]*fun_x1(x[k][0],x[k][1]), x[k][1] - a[1]*fun_x2(x[k][0],x[k][1])) < fun(x[k][0],x[k][1]) - (a[0]*beta*fun_x1(x[k][0],x[k][1]) + a[1]*beta*fun_x2(x[k][0],x[k][1])):
+        a[0] *= tow
+        a[1] *= tow
+        l+=1
+    alpha.append(a)
+    x.append((x[-1][0]-a[0]*fun_x1(x[k][0],x[k][1]), x[-1][1]-a[1]*fun_x2(x[k][0],x[k][1])))
+    k+=1
+
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+# a = np.linspace(0,k,k)
+# b = x[a][0]
+# c = x[a][1]
+p = x[:, 0]
+q = x[:, 1]
+r = fun(p,q)
+ax.plot3D(p, q, r, 'green')
+#ax.set_title('3D line plot geeks for geeks')
+plt.show()
+
+print("Took ", k, " iterations to converge.")
---
+++ 3.py
@@ -0,0 +1,58 @@
+import numpy as np
+from numpy.linalg import eigh
+import random
+import matplotlib.pyplot as plt
+
+def fun(a,b):
+    return 5*(a**2 + b**2) - a*b - 11*a + 11*b + 11
+
+def grad_fun(a,b):
+    return (10*a- b - 11, 10*b - a + 11)
+
+#Equation is 5x1**2 + 5x2**2 -x1x2 - 11x1 + 11x2 + 11
+hess = [[10,-1],[-1,10]]
+eval, evec = eigh(np.array(hess))
+largest = max(eval)
+alpha = [random.uniform(0, 2/largest) for i in range(5)]
+eps = 1e-6
+
+for i in range(5):
+    x = [(1,0)]
+    k = 0
+    f = [fun(x[0][0], x[0][1])]
+    while grad_fun(x[-1][0], x[-1][1])[0]**2 + grad_fun(x[-1][0], x[-1][1])[1]**2 > eps**2:
+        alpha_temp = alpha[i]
+        x_new = (x[-1][0] - alpha_temp*(grad_fun(x[-1][0], x[-1][1])[0]), x[-1][1] - alpha[i]*(grad_fun(x[-1][0], x[-1][1])[1]))
+        x.append(x_new)
+        k+=1
+        f.append(fun(x_new[0], x_new[1]))
+    [p, q] = np.meshgrid(x[:][0], x[:][1])
+    fig, ax = plt.subplots(1, 1)
+    r = fun(p,q)
+    ax.contour(p,q,r)
+    ax.set_title('Contour Plot')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    plt.show()
+    print("Took ", k, " iterations to converge when alpha is ", alpha[i])
+
+alpha2 = random.uniform(2/largest, 1)
+for i in range(1):
+    x = [(1,0)]
+    k = 0
+    f = [fun(x[0][0], x[0][1])]
+    while grad_fun(x[-1][0], x[-1][1])[0]**2 + grad_fun(x[-1][0], x[-1][1])[1]**2 > eps**2:
+        alpha_temp = alpha2
+        x_new = (x[-1][0] - alpha_temp*(grad_fun(x[-1][0], x[-1][1])[0]), x[-1][1] - alpha[i]*(grad_fun(x[-1][0], x[-1][1])[1]))
+        x.append(x_new)
+        k+=1
+        f.append(fun(x_new[0], x_new[1]))
+    [p, q] = np.meshgrid(x[:][0], x[:][1])
+    fig, ax = plt.subplots(1, 1)
+    r = fun(p,q)
+    ax.contour(p,q,r)
+    ax.set_title('Contour Plot')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    plt.show()
+    print("Took ", k, " iterations to converge when alpha is ", alpha2)
